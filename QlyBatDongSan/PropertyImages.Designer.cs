5767bfeeb25881a594449db144d2d97da4ea28c1
vanha1108 <nguyenvanha11899@gmail.com>
2021-06-07 23:55:52 +0700

    UP FN

--- QlyBatDongSan/PropertyImages.Designer.cs
+++ QlyBatDongSan/PropertyImages.Designer.cs
@@ -222,6 +222,7 @@ namespace QlyBatDongSan
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PropertyImages";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Property Images";
             this.Load += new System.EventHandler(this.PropertyImages_Load);
             this.panel1.ResumeLayout(false);
